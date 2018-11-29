33881368d7f962a997e326d3926ed15defcf41d9
jaywcjlove <398188662@qq.com>
2018-11-29 12:58:07 +0800

    Add DynamicLoadablePlugin

---
+++ DynamicLoadablePlugin.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+function buildManifest(compiler, compilation) {
+  const context = compiler.options.context;
+  const manifest = {};
+
+  compilation.chunks.forEach((chunk) => {
+    chunk.files.forEach((file) => {
+      chunk.forEachModule((module) => {
+        const id = module.id;
+        const name = typeof module.libIdent === 'function' ? module.libIdent({ context }) : null;
+        const publicPath = url.resolve(compilation.outputOptions.publicPath || '', file);
+
+        let currentModule = module;
+        if (module.constructor.name === 'ConcatenatedModule') {
+          currentModule = module.rootModule;
+        }
+        if (!manifest[currentModule.rawRequest]) {
+          manifest[currentModule.rawRequest] = [];
+        }
+
+        manifest[currentModule.rawRequest].push({ id, name, file, publicPath });
+      });
+    });
+  });
+
+  return manifest;
+}
+
+class DynamicLoadablePlugin {
+  constructor(opts = {}) {
+    this.filename = opts.filename;
+  }
+
+  apply(compiler) {
+    compiler.plugin('emit', (compilation, callback) => {
+      const manifest = buildManifest(compiler, compilation);
+      const json = JSON.stringify(manifest, null, 2);
+      const outputDirectory = path.dirname(this.filename);
+      try {
+        fs.mkdirSync(outputDirectory);
+      } catch (err) {
+        if (err.code !== 'EEXIST') {
+          throw err;
+        }
+      }
+      fs.writeFileSync(this.filename, json);
+      callback();
+    });
+  }
+}
+
+function getBundles(manifest, moduleIds) {
+  return moduleIds.reduce((bundles, moduleId) => {
+    return bundles.concat(manifest[moduleId]);
+  }, []);
+}
+
+exports.DynamicLoadablePlugin = DynamicLoadablePlugin;
+exports.getBundles = getBundles;
