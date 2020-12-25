5f497ef42de90e74b2ce9a919de0fdb86c984642
jaywcjlove <398188662@qq.com>
2020-12-25 23:23:48 +0800

    feat: support typescript.

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+type Options = {
+  models: Promise<any>[];
+  component: Promise<any>[];
+  LoadingComponent: JSX.Element;
+}
+
+declare function dynamicLoadable(options: Options): React.ReactNode;
+declare class DynamicLoadable {
+  setDefaultLoadingComponent(LoadingComponent: React.ReactNode): void;
+}
+
+declare let dl: typeof dynamicLoadable & DynamicLoadable;
+export default dl;
